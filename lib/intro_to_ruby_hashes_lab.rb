ffe8fd380f64571c8ba6f1257549d28183d81f12
Christopher Henderson <chenderson5280@gmail.com>
2020-09-05 02:53:51 +0000

    Automatically backed up by Learn

--- lib/intro_to_ruby_hashes_lab.rb
+++ lib/intro_to_ruby_hashes_lab.rb
@@ -9,10 +9,10 @@ return hash[key]
 end
 
 def update_counting_hash(hash, key)
-  if new_hash[key]
-    new_hash [key]+=1
+  if hash[key]
+    hash [key]+=1
   else
-    new_hash[key] = 1
+    hash[key] = 1
 end
 end
 # given a hash an a key as parameters, return an updated hash
